7930542c81fb30c56631caf1255ad3f0ca3d0de8
HendrikVanBeersel <hendrikvanbeersel@hotmail.com>
2023-07-30 20:11:45 +0200

    removing windows endings

--- update-server-commands.sh
+++ update-server-commands.sh
@@ -7,11 +7,21 @@ if [[ ${UID} != 0 ]]; then
     "
     exit 1
 fi
+echo "removing windows line endings"
+sed -i 's/\r$//' stop-minecraft.sh
+sed -i 's/\r$//' start-minecraft.sh
+sed -i 's/\r$//' backup-minecraft.sh
+sed -i 's/\r$//' update-server-commands.sh
+
+echo "giving execute permissions to scripts"
+chmod +x stop-minecraft.sh
+chmod +x start-minecraft.sh
+chmod +x backup-minecraft.sh
+chmod +x update-server-commands.sh
 
 echo "copying files to /usr/local/bin"
 cp stop-minecraft.sh /usr/local/bin/stop-minecraft
 cp start-minecraft.sh /usr/local/bin/start-minecraft
 cp backup-minecraft.sh /usr/local/bin/backup-minecraft
-
 cp update-server-commands.sh /usr/local/bin/update-server-commands
 
